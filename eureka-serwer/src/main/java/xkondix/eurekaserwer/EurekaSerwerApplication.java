cc0a0f7697f2496a72e9e18e0d07a791179ec710
Konrad <konrad.kowalczyk.98@gmail.com>
2023-04-09 19:57:31 +0200

    init

---
+++ eureka-serwer/src/main/java/xkondix/eurekaserwer/EurekaSerwerApplication.java
@@ -0,0 +1,13 @@
+package xkondix.eurekaserwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSerwerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSerwerApplication.class, args);
+    }
+
+}

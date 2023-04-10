2882db74253b6aaa4936244ad2004857780bded1
Konrad <konrad.kowalczyk.98@gmail.com>
2023-04-10 12:45:38 +0200

    master - rename EurekaServerApplication

--- eureka-server/src/main/java/xkondix/eurekaserwer/EurekaServerApplication.java
+++ eureka-server/src/main/java/xkondix/eurekaserwer/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaSerwerApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaSerwerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }

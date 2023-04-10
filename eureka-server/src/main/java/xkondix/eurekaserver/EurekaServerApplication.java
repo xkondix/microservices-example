6c121d9610ffc40a32bd63c0dd1a56b238607fe0
Konrad <konrad.kowalczyk.98@gmail.com>
2023-04-10 16:05:47 +0200

    master - add EurekaServerApplication

--- eureka-server/src/main/java/xkondix/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/xkondix/eurekaserver/EurekaServerApplication.java
@@ -1,11 +1,9 @@
-package xkondix.eurekaserwer;
+package xkondix.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- eureka-server/src/test/java/xkondix/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/xkondix/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package xkondix.eurekaserwer;
+package xkondix.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
